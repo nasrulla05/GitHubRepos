676e2549ac80969f62608883f95500d4a8f6cb00
Nasrulla Akhbulatov <nasrulla.akhbulatov@gmail.com>
2023-03-03 22:23:18 +0300

    Добавил библиотеку Cicerone

--- app/src/main/java/com/akhbulatov/githubrepos/GitHubReposApplication.kt
+++ app/src/main/java/com/akhbulatov/githubrepos/GitHubReposApplication.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import androidx.room.Room
 import com.akhbulatov.githubrepos.database.AppDatabase
 import com.akhbulatov.githubrepos.network.GitHubService
+import com.github.terrakok.cicerone.Cicerone
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.Router
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -24,10 +27,18 @@ class GitHubReposApplication : Application() {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         gitHubService = retrofit.create()
+
+        cicerone = Cicerone.create()
+        router = cicerone.router
+        navigateHolder = cicerone.getNavigatorHolder()
     }
 
     companion object {
         lateinit var appDatabase: AppDatabase
         lateinit var gitHubService: GitHubService
+
+        private lateinit var cicerone: Cicerone<Router>
+        lateinit var router: Router
+        lateinit var navigateHolder: NavigatorHolder
     }
 }
