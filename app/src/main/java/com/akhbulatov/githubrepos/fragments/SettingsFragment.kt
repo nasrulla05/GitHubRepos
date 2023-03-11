0d089c06442f61516df2feee717add52890e6395
Nasrulla Akhbulatov <nasrulla.akhbulatov@gmail.com>
2023-03-11 10:04:14 +0300

    Создал ViewModel для фрагмента настроек

--- app/src/main/java/com/akhbulatov/githubrepos/fragments/SettingsFragment.kt
+++ app/src/main/java/com/akhbulatov/githubrepos/fragments/SettingsFragment.kt
@@ -1,54 +1,49 @@
 package com.akhbulatov.githubrepos.fragments
 
-import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.akhbulatov.githubrepos.GitHubReposApplication
 import com.akhbulatov.githubrepos.R
 import com.akhbulatov.githubrepos.databinding.FragmentSettingsBinding
+import com.akhbulatov.githubrepos.viewModel.SettingsViewModel
 import com.google.android.material.snackbar.Snackbar
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
     var binding : FragmentSettingsBinding? = null
+    val viewModel:SettingsViewModel by lazy {
+        ViewModelProvider(this).get(SettingsViewModel::class.java)
+    }
+    lateinit var repositories:EditText
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSettingsBinding.bind(view)
+        repositories = view.findViewById(R.id.repositories)
 
         binding!!.settingsToolbar.setNavigationOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 GitHubReposApplication.router.exit()
             }
         })
+        viewModel.repositoriesEditor()
 
-        val sharedPreferences = requireContext().getSharedPreferences(
-            "git_hub_preferences",
-            Context.MODE_PRIVATE
-        )
-
-        val repositoriesEditor: String? =
-            sharedPreferences.getString("repositories", null)
-        binding!!.repositories.setText(repositoriesEditor)
+        viewModel.repositoriesLiveData.observe(viewLifecycleOwner){t->
+            binding!!.repositories.setText(t)
+        }
 
         binding!!.save.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 saveSettings()
             }
         })
+
+        viewModel.loadSaveSettings()
     }
 
     private fun saveSettings() {
-        val sharedPreferences = requireContext().getSharedPreferences(
-            "git_hub_preferences",
-            Context.MODE_PRIVATE
-        )
-        val repositoriesList = binding!!.repositories.text.toString()
-
-        val editor: SharedPreferences.Editor = sharedPreferences.edit()
-        editor.putString("repositories", repositoriesList)
-        editor.apply()
 
         val snackbar = Snackbar.make(
             requireView(),
---
+++ app/src/main/java/com/akhbulatov/githubrepos/viewModel/SettingsViewModel.kt
@@ -0,0 +1,35 @@
+package com.akhbulatov.githubrepos.viewModel
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.akhbulatov.githubrepos.GitHubReposApplication
+
+class SettingsViewModel:ViewModel() {
+
+    val repositoriesLiveData = MutableLiveData<String>()
+
+    fun repositoriesEditor(){
+        val sharedPreferences = GitHubReposApplication.context.getSharedPreferences(
+            "git_hub_preferences",
+            Context.MODE_PRIVATE
+        )
+        val repositoriesEditor: String =
+            sharedPreferences.getString("repositories", null)!!
+        repositoriesLiveData.value = repositoriesEditor
+    }
+
+    fun loadSaveSettings(){
+        val sharedPreferences = GitHubReposApplication.context.getSharedPreferences(
+            "git_hub_preferences",
+            Context.MODE_PRIVATE
+        )
+        val repositoriesList = repositoriesLiveData.value.toString()
+
+        val editor: SharedPreferences.Editor = sharedPreferences.edit()
+        editor.putString("repositories", repositoriesList)
+        editor.apply()
+    }
+
+}
